486fb0fa69cae74c2a9aa33c672316f8e6e5c192
nicolasarruda <nlarruda@hotmail.com>
2022-10-24 21:59:49 -0300

    Frontweb

---
+++ frontweb/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import './assets/styles/custom.scss';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="app-container">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontweb/src/components/Navbar/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+import '../../assets/styles/custom.scss';
+
+export const Navbar = () => {
+  return (
+    <div className="navbar-container">
+      <h1 className="navbar-title">BDS vendas</h1>
+    </div>
+  );
+};
+
+export default Navbar;
